1ad5245122cae82a0064202c36fcd3b2cbed99b8
rkiel <cygnus@pobox.com>
2016-01-31 17:10:39 -0500

    jwt: modularize csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
+require 'angular_csrf_protection'
+
 class ApplicationController < ActionController::Base
+
+  include AngularCsrfProtection
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   after_filter :set_csrf_cookie_for_ng
-
-  def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-  end
 
   def current_user
     @current_user ||= User.find_by(uuid: session[:user_uuid]) if session[:user_uuid]
@@ -19,9 +20,4 @@ class ApplicationController < ActionController::Base
     redirect_to hornburg_approach_path unless current_user
   end
 
-  protected
-
-  def verified_request?
-    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
-  end
 end
